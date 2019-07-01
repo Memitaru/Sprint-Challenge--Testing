d499b4b7acf91233d1d69e98d6892a5076034ad5
Memitaru <amandatwah@gmail.com>
2019-06-30 18:40:34 -0700

    Completed self study questions

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const gamesRoutes = require('../routes/gamesRoutes');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/games', gamesRoutes)
 
 server.get('/', (req, res) => {
     res.status(200).json({message: "API is Up"})
