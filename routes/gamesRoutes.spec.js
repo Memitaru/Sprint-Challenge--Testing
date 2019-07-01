39197c88bcc452ad54e4c261adf4913c65ef607f
Memitaru <amandatwah@gmail.com>
2019-06-30 19:25:22 -0700

    All tests writteN

--- routes/gamesRoutes.spec.js
+++ routes/gamesRoutes.spec.js
@@ -30,9 +30,43 @@ describe('Games API', () => {
     })
 
     describe('/games /POST', () => {
+
         it('returns status 422 if required fields are missing', async () => {
             const newGame = {title: "Final Fantasy XIV"}
-            const res = await request(server)
+            const res = await request(server).post('/games').send(newGame)
+
+            expect(res.status).toBe(422);
+        })
+
+        it('will return a status 201 if item is added successfully', async () => {
+            const newGame = {
+                title: "Final Fantasy XIV",
+                genre: "RPG",
+                releaseYear: 2010
+            }
+
+            const res = await request(server).post('/games').send(newGame)
+
+            expect(res.status).toBe(201)
+        })
+
+        it('will return a status 405 if trying to add a duplicate game title', async () => {
+            const newGame = {
+                title: "Final Fantasy XI",
+                genre: "RPG",
+                releaseYear: 2002
+            }
+
+            const double = {
+                title: "Final Fantasy XI",
+                genre: "MMO",
+                releaseYear: 2002
+            }
+
+            const res = await request(server).post('/games').send(newGame)
+            const doubleRes = await request(server).post('/games').send(double)
+
+            expect(doubleRes.status).toBe(405)
         })
     })
 })
