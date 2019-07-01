749e43a5fac9016cc39c7e84535aee7120106ffb
Memitaru <amandatwah@gmail.com>
2019-06-30 19:50:15 -0700

    MVP plus a little stretch

--- routes/gamesRoutes.js
+++ routes/gamesRoutes.js
@@ -12,8 +12,26 @@ router.get('/', (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    try{
+        if (req.body.title && req.body.genre){
+            const gameTitles = db.map(game => game.title.toLowerCase())
+
+            const checkDouble = gameTitles.includes(req.body.title.toLowerCase())
+
+            if(checkDouble){
+                res.status(405).json({message: "Duplicate title"})
+            } else {
+                db.push(req.body)
+                res.status(201).send(req.body)
+            }
+        } else {
+            res.status(422).json({message: "Must include title and genre"})
+        }
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
 })
 
 module.exports = router;
